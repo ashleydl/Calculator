f200463ac8d2b22e3ea14b6e27dbd0eb08227dfd
ashleydl <loof0032@hz.nl>
2017-11-01 14:19:48 +0100

    first changes

---
+++ Calculator/Calculator2/ashleystestje.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public class AshleysTestje : CalculatorMusthaves
+    {
+        public int Diff(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Product(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Quotient(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Sum(int getal1, int getal2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
